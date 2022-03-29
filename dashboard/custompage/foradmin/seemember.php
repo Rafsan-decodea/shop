6289074141777ce17ecb9dfb27e8aa5fe3df9192
MD Rafsan <shazidno123@gmail.com>
2022-03-29 13:42:12 +0600

    add /

--- dashboard/custompage/foradmin/seemember.php
+++ dashboard/custompage/foradmin/seemember.php
@@ -175,7 +175,7 @@ if( $_SESSION["uid"]==0)
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
               <li class="breadcrumb-item active">See Members</li>
             </ol>
           </div><!-- /.col -->
--- dashboard/custompage/forusers/seeproduct.php
+++ dashboard/custompage/forusers/seeproduct.php
@@ -269,7 +269,7 @@ if($_SESSION["uid"]==1)
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v1</li>
             </ol>
           </div><!-- /.col -->
