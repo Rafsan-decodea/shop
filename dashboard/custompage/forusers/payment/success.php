324d8861aac5206cd9d464a8ff99eb6f3365bbd0
Your Name <you@example.com>
2022-04-14 10:59:44 +0600

    add Payment

--- dashboard/custompage/forusers/payment/success.php
+++ dashboard/custompage/forusers/payment/success.php
@@ -7,6 +7,18 @@ include $_SERVER['DOCUMENT_ROOT'] . "/shop/database/db.php";
 
 
 
+$db = new DB();
+
+if(isset($_SESSION["id"])){
+
+$uid = $_SESSION["id"];
+
+$sql = "UPDATE shop_orders set acceptrequest = 1 WHERE userid = $uid ";
+$db->query($sql);
+header("location:/shop/dashboard/custompage/forusers/seeproduct.php");
+
+}
+header("location:/shop/dashboard/custompage/forusers/seeproduct.php");
 
 
 ?>
